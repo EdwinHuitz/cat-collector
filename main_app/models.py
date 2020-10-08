c4b205b7b552b3cea71b2dfb7404be4586c144a8
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-10-08 11:46:31 -0400

    adding ability to create toys and associate them with cat

--- main_app/models.py
+++ main_app/models.py
@@ -1,24 +1,34 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-# Create your models here.
-class Cat(models.Model):
-  name = models.CharField(max_length=100)
-  breed = models.CharField(max_length=100)
-  description = models.TextField(max_length=250)
-  age = models.IntegerField()
 
-  def __str__(self):
-    return self.name
-  def get_absolute_url(self):
-    return reverse('detail',kwargs={'cat_id':self.id})
-  def fed_for_today(self):
-    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 MEALS = (
   ('B', 'Breakfast'),
   ('L', 'Lunch'),
   ('D', 'Dinner'),
 )
+# Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+  def __str__(self):
+    return self.name
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+class Cat(models.Model):
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
+  def __str__(self):
+    return self.name
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'cat_id': self.id})
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
 class Feeding(models.Model):
   date = models.DateField('feeding date')
   meal = models.CharField(
@@ -31,3 +41,4 @@ class Feeding(models.Model):
     return f"{self.get_meal_display()} on {self.date}"
   class Meta:
     ordering = ['-date']
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from .models import Cat
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
 from .forms import FeedingForm
-from django.shortcuts import render,redirect
 # Create your views here.
 
 def home(request):
@@ -18,8 +17,22 @@ def cats_index(request):
 
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
+  # Get the toys the cat doesn't have
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
   feeding_form = FeedingForm()
-  return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form })
+  return render(request, 'cats/detail.html', {
+    'cat': cat, 'feeding_form': feeding_form,
+    # Add the toys to be displayed
+    'toys': toys_cat_doesnt_have
+  })
+def assoc_toy(request, cat_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+def delete_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.remove(toy_id)
+  return redirect('detail', cat_id=cat_id)
 
 def add_feeding(request, cat_id):
   form=FeedingForm(request.POST)
@@ -42,3 +55,17 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
   model = Cat
   success_url = '/cats/'
+
+class ToyList(ListView):
+  model = Toy
+class ToyDetail(DetailView):
+  model = Toy
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
