cb2380b5bf909e362442e454576d54c05d0d9a0f
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-10-09 15:28:03 -0400

    adding authentication

--- main_app/migrations/0004_cat_toys.py
+++ main_app/migrations/0004_cat_toys.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main_app', '0003_auto_20201008_1011'),
+        ('main_app', '0003_auto_20201008_1411'),
     ]
 
     operations = [
--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 MEALS = (
   ('B', 'Breakfast'),
   ('L', 'Lunch'),
-  ('D', 'Dinner'),
+  ('D', 'Dinner')
 )
+
+
 # Create your models here.
+
 class Toy(models.Model):
   name = models.CharField(max_length=50)
   color = models.CharField(max_length=20)
+
   def __str__(self):
     return self.name
+
   def get_absolute_url(self):
     return reverse('toys_detail', kwargs={'pk': self.id})
 
@@ -22,10 +28,14 @@ class Cat(models.Model):
   description = models.TextField(max_length=250)
   age = models.IntegerField()
   toys = models.ManyToManyField(Toy)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
   def __str__(self):
     return self.name
+
   def get_absolute_url(self):
     return reverse('detail', kwargs={'cat_id': self.id})
+
   def fed_for_today(self):
     return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
@@ -35,10 +45,19 @@ class Feeding(models.Model):
     max_length=1,
     choices=MEALS,
     default=MEALS[0][0]
-    )
+  )
+
   cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
   def __str__(self):
     return f"{self.get_meal_display()} on {self.date}"
+  
   class Meta:
     ordering = ['-date']
-  
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for cat_id: {self.cat_id} @{self.url}"
